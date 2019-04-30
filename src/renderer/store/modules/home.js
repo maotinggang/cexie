7c3098bece535a4131cfa1c24d40433fb326be92
microgram <maogang2014@126.com>
2019-04-30 22:55:27 +0800

    update

--- src/renderer/store/modules/home.js
+++ src/renderer/store/modules/home.js
@@ -1,5 +1,6 @@
 import collection from 'lodash/collection'
 import * as xlsx from 'xlsx'
+import * as fs from 'fs'
 const state = {
   windowSize: { height: window.innerHeight, width: window.innerWidth },
   changeData: '',
@@ -25,9 +26,11 @@ const actions = {
       width: window.innerWidth
     })
   },
-  actionsReadFile({ commit }, value) {
-    let workbook = xlsx.readFile(value)
-    value = xlsx.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]])
+  actionsReadFile({ commit }, filename) {
+    let workbook = xlsx.readFile(filename)
+    let value = xlsx.utils.sheet_to_json(
+      workbook.Sheets[workbook.SheetNames[0]]
+    )
     let maxChange = 0
     for (let i = 0; i < value.length; i++) {
       value[i].x =
@@ -45,6 +48,9 @@ const actions = {
       if (Math.abs(value[i].x) > maxChange) maxChange = Math.abs(value[i].x)
       if (Math.abs(value[i].y) > maxChange) maxChange = Math.abs(value[i].y)
     }
+    let test = xlsx.utils.json_to_sheet(value)
+    var stream = xlsx.stream.to_csv(test)
+    stream.pipe(fs.createWriteStream(filename + '.csv'))
     commit('MAX_CHANGE', maxChange)
     commit('READ_FILE', collection.sortBy(value, ['name', 'time', 'depth']))
   }
